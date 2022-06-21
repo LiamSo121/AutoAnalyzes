908f7050bd0ef47952f39eb4649f2fd424677287
LiamSo121 <liam121300@gmail.com>
2022-06-22 02:02:47 +0300

    Create sqlIntegration.py

---
+++ sqlIntegration.py
@@ -0,0 +1,29 @@
+import psycopg2
+from psycopg2 import Error
+
+try:
+    # Connect to an existing database
+    connection = psycopg2.connect(user="postgres",
+                                  password="1234",
+                                  host="localhost",
+                                  port="5432",
+                                  database="gap_reversal")
+
+    # Create a cursor to perform database operations
+    cursor = connection.cursor()
+    # Print PostgreSQL details
+    print("PostgreSQL server information")
+    print(connection.get_dsn_parameters(), "\n")
+    # Executing a SQL query
+    cursor.execute("SELECT symbol, FROM orders WHERE time BETWEEN  GROUP BY pl;")
+    # Fetch result
+    record = cursor.fetchall() 
+    print(record)
+
+except (Exception, Error) as error:
+    print("Error while connecting to PostgreSQL", error)
+finally:
+    if (connection):
+        cursor.close()
+        connection.close()
+        print("PostgreSQL connection is closed")
