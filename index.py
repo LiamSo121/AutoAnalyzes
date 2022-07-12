4681be978640d3ca2f7404fd0c8c485702a2f63b
LiamSo121 <liam121300@gmail.com>
2022-07-12 21:46:40 +0300

    #recafor

--- index.py
+++ index.py
@@ -3,9 +3,9 @@ import numpy as np
 from helpers.AnalyzeHelpers import AnalizeHelpers
 from Processes.Visualization import Visual
 
-visual = Visual()
-pd.options.mode.chained_assignment = None  # default='warn'
 
+pd.options.mode.chained_assignment = None  # default='warn'
+visual = Visual()
 analize_helper = AnalizeHelpers()
 # Input xlsx file
 summaryOrigin = pd.read_excel("continues.xlsx")
@@ -16,21 +16,21 @@ risk = 0.009
 # Define Starting Fund
 fund = 30960
 
-# # Stage 1 - Analyze daily positions
-# summary = analize_helper.add_daily_change(summary,risk,fund)
-# # Stage 2 - Analize Monthly
-# yearlySum,by_period_df,half_hour_hit_percantage,hourly_hit_percantage = analize_helper.calc_yearly(summary)
-# # Stage 3 - Grouping By
-# groupByType,profitsBy30Min,losesBy30Min = analize_helper.group_by(summary)
-# # Stage 4 - Export all the data to xlsx file
-# export_list = [summary,yearlySum,groupByType,profitsBy30Min,losesBy30Min,by_period_df,half_hour_hit_percantage,hourly_hit_percantage]
-# analize_helper.export_to_excel(export_list)
+# Stage 1 - Analyze daily positions
+summary = analize_helper.add_daily_change(summary,risk,fund)
+# Stage 2 - Analize Monthly
+yearlySum,by_period_df,half_hour_hit_percantage,hourly_hit_percantage = analize_helper.calc_yearly(summary)
+# Stage 3 - Grouping By
+groupByType,profitsBy30Min,losesBy30Min = analize_helper.group_by(summary)
+# Stage 4 - Export all the data to xlsx file
+export_list = [summary,yearlySum,groupByType,profitsBy30Min,losesBy30Min,by_period_df,half_hour_hit_percantage,hourly_hit_percantage]
+analize_helper.export_to_excel(export_list)
 
-# stage 5 - Visualization
-# visual.half_hour_plot()
-# visual.hour_plot()
-# visual.sum_of_positions_by_time()
-# visual.hit_percentage_by_month_plot()
+#stage 5 - Visualization
+visual.half_hour_plot()
+visual.hour_plot()
+visual.sum_of_positions_by_time()
+visual.hit_percentage_by_month_plot()
 visual.sum_of_positions_by_type()
 
 
