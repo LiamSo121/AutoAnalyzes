cf7dcde5185b51182dcfd7376ac0d1ec361ad6ed
LiamSo121 <liam121300@gmail.com>
2022-07-13 20:15:56 +0300

    # add hit perc by  month by time

--- index.py
+++ index.py
@@ -16,19 +16,20 @@ risk = 0.005
 # Define Starting Fund
 fund = 30960
 
-# Stage 1 - Analyze daily positions
-summary = analize_helper.add_daily_change(summary,risk,fund)
-# Stage 2 - Analize Monthly
-yearlySum,by_period_df,half_hour_hit_percantage,hourly_hit_percantage = analize_helper.calc_yearly(summary)
-# Stage 3 - Grouping By
-groupByType,profitsBy30Min,losesBy30Min = analize_helper.group_by(summary)
-# Stage 4 - Export all the data to xlsx file
-export_list = [summary,yearlySum,groupByType,profitsBy30Min,losesBy30Min,by_period_df,half_hour_hit_percantage,hourly_hit_percantage]
-analize_helper.export_to_excel(export_list)
-#stage 5 - Visualization
-analize_helper.visualize()
+# # Stage 1 - Analyze daily positions
+# summary = analize_helper.add_daily_change(summary,risk,fund)
+# # Stage 2 - Analize Monthly
+# yearlySum,by_period_df,half_hour_hit_percantage,hourly_hit_percantage = analize_helper.calc_yearly(summary)
+# # Stage 3 - Grouping By
+# groupByType,profitsBy30Min,losesBy30Min = analize_helper.group_by(summary)
+# # Stage 4 - Export all the data to xlsx file
+# export_list = [summary,yearlySum,groupByType,profitsBy30Min,losesBy30Min,by_period_df,half_hour_hit_percantage,hourly_hit_percantage]
+# analize_helper.export_to_excel(export_list)
+# #stage 5 - Visualization
+# analize_helper.visualize()
 
 
 
 
 
+visual.hit_percentage_month_time()
