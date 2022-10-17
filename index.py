883cc36cba6e5aa7a857b42958c44f2b7348c378
LiamSo121 <liam121300@gmail.com>
2022-10-17 22:53:31 +0300

    done

--- index.py
+++ index.py
@@ -2,37 +2,30 @@ import pandas as pd
 from helpers.AnalyzeHelpers import AnalizeHelpers
 from Processes.Visualization import Visual
 from os import walk
+import numpy as np
 import csv
 
 
 pd.options.mode.chained_assignment = None  # default='warn'
 visual = Visual()
 analize_helper = AnalizeHelpers()
-path = './Excel_files'
+path = './Outputs'
 file_names = next(walk(path),(None,None,[]))[2]
 # Define risk percantage
 risk = 0.002
 # Define Starting Fund
 fund = 100000
+
+df = pd.DataFrame(columns=['Symbol','Total Positions','Profits','Losses','Hit Percentage'])
 for filename in file_names:
-    output_file_name = filename + '-Analyzed'
-    summaryOrigin = pd.read_csv(f"Excel_files\\{filename}")
-    
-    summary = summaryOrigin.copy()
-    # Stage 1 - Analyze daily positions
-    summary = analize_helper.add_daily_change(summary,risk,fund)
-    # Stage 2 - Analize Monthly
-    yearlySum,half_hour_hit_percantage,hourly_hit_percantage = analize_helper.calc_yearly(summary,fund)
-    # Stage 3 - Grouping By
-    groupByType,profitsBy30Min,losesBy30Min,groupBySymbol,groupByGap = analize_helper.group_by(summary)
-    # Stage 4 - Export all the data to xlsx file
-    export_list = [summary,yearlySum,groupByType,profitsBy30Min,losesBy30Min,half_hour_hit_percantage,hourly_hit_percantage,groupBySymbol,groupByGap]
-    analize_helper.export_to_excel(export_list,output_file_name)
-    #stage 5 - Visualization
-    #analize_helper.visualize(output_file_name)
-    print(f'{filename} Is done :)')
+    summaryOrigin = pd.read_excel(f"Outputs\\{filename}",sheet_name='Hit By Symbol')
+    summaryOrigin = summaryOrigin[summaryOrigin['Hit Percentage'] > 50]
+    df = pd.concat([df,summaryOrigin])
+print(df)
 
+table = pd.pivot_table(df,values=['Total Positions','Profits','Losses','Hit Percentage'],index=['Symbol'],
+                        aggfunc={'Total Positions': np.sum,'Profits':np.sum,'Losses':np.sum,'Hit Percentage':np.mean}).reset_index()
 
-print("All files have been analyzed")
-
-
+print(list(table['Symbol']))
+table.to_excel('stocks-sl0.02.xlsx')
+print('done')
