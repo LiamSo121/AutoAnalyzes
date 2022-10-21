df89128a4a8af3316b36ae59c0255bd1d13576a8
LiamSo121 <liam121300@gmail.com>
2022-10-21 17:10:41 +0300

    fix

--- index.py
+++ index.py
@@ -1,3 +1,4 @@
+from pickle import FALSE
 import pandas as pd
 from helpers.AnalyzeHelpers import AnalizeHelpers
 from Processes.Visualization import Visual
@@ -19,13 +20,15 @@ fund = 100000
 df = pd.DataFrame(columns=['Symbol','Total Positions','Profits','Losses','Hit Percentage'])
 for filename in file_names:
     summaryOrigin = pd.read_excel(f"Outputs\\{filename}",sheet_name='Hit By Symbol')
-    summaryOrigin = summaryOrigin[summaryOrigin['Hit Percentage'] > 50]
+    summaryOrigin = summaryOrigin[summaryOrigin['Hit Percentage'] > 55]
     df = pd.concat([df,summaryOrigin])
-print(df)
+
 
 table = pd.pivot_table(df,values=['Total Positions','Profits','Losses','Hit Percentage'],index=['Symbol'],
                         aggfunc={'Total Positions': np.sum,'Profits':np.sum,'Losses':np.sum,'Hit Percentage':np.mean}).reset_index()
 
-print(list(table['Symbol']))
-table.to_excel('stocks-sl0.02.xlsx')
+df = table.reindex(table.sort_values(by=['Hit Percentage','Total Positions'], ascending=[False,False]).index)
+print(df)
+print(list(df['Symbol']))
+table.to_excel('stocks_all_years_over55.xlsx')
 print('done')
